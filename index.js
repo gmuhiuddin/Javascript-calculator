56b88f92d509450fd9bf210be70388c5bc814690
Ghulam muhiuddin <muhiuddinghulam825@gmail.com>
2024-04-13 00:27:59 +0500

    add some blaa blaa blaa

--- index.js
+++ index.js
@@ -3,25 +3,25 @@ let buttons = document.querySelectorAll('button');
 
 
 let string = "";
-// let arr = buttons;
-buttons.forEach(button =>{
-    button.addEventListener('click',(e) =>{
-        if(e.target.innerHTML == '='){
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
             string = eval(string);
-            input.value = string; 
+            input.value = string;
         }
 
-        else if (e.target.innerHTML == 'AC'){
+        else if (e.target.innerHTML == 'AC') {
             string = "";
             input.value = string;
         }
-        else if (e.target.innerHTML == 'DEL'){
-            string = string.slice(0,string.length-1)
+        else if (e.target.innerHTML == 'DEL') {
+            string = string.slice(0, string.length - 1)
             input.value = string
         }
-        else{
+        else {
             string += e.target.innerHTML;
             input.value = string;
-        }     
+        }
     })
 })
